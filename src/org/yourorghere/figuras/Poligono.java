b094e6a138f3833354a69c05acffc221a1488c5e
LLealL <llealdegoes@gmail.com>
2018-09-15 19:08:32 -0300

    desbugando o projeto

---
+++ src/org/yourorghere/figuras/Poligono.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.yourorghere.figuras;
+
+import org.yourorghere.figuras.Util.RGB;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import javax.media.opengl.GL;
+
+/**
+ *
+ * @author keyalisth
+ */
+public class Poligono extends Figura {
+    
+    private int lados;
+
+    
+    public Poligono(int n,int x, int y,RGB cor,Integer id){
+        super(id,x,y,cor);
+        lados=n;
+    }
+        
+    
+    @Override
+    public void desenhar(GL gl){
+        double angulo;
+        gl.glTranslatef(-1.5f, -2.0f, -3.0f);
+        gl.glBegin(GL.GL_POLYGON);
+        gl.glColor3f(super.getCor().getRed(), super.getCor().getGreen(), super.getCor().getBlue());
+        for(int i=0;i<lados;++i){
+            angulo= (2*Math.PI*i/lados);
+            gl.glVertex2d(Math.cos(angulo)*(super.getX()+5),Math.sin(angulo)*(super.getY()+5));
+        }
+        gl.glEnd();
+    }
+
+
+
+}
+
---
+++ src/org/yourorghere/figuras/Triangulo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.yourorghere.figuras;
+
+import javax.media.opengl.GL;
+import static javax.media.opengl.GL.GL_TRIANGLES;
+import org.yourorghere.figuras.Util.RGB;
+
+/**
+ *
+ * @author keyalisth
+ */
+public class Triangulo extends Figura{
+
+    public Triangulo(Integer id, int x, int y, RGB cor) {
+        super(id, x, y, cor);     
+    }
+
+
+    @Override
+    public void desenhar(GL gl) {
+        gl.glTranslatef(-1.5f, -2.0f, -3.0f);
+        gl.glBegin(GL_TRIANGLES);
+            gl.glColor3f(super.getCor().getRed(),super.getCor().getGreen(),super.getCor().getBlue());
+            gl.glVertex2f(super.getX()-3,super.getY()-2);
+            gl.glVertex2f(super.getX(),super.getY()+2);
+            gl.glVertex2f(super.getX()+3,super.getY()-2);
+        gl.glEnd();
+    }
+
+
+    
+    
+}
